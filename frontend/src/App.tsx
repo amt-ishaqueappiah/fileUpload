81501f4834b47356393074e53348c7de7952d416
Ishaque <108622096+Ishaque22@users.noreply.github.com>
2023-04-29 14:25:57 +0000

    hhhhh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,34 +4,47 @@ import axios from 'axios';
 
 function App() {
 
-  const[allFiles, setAllFiles] = useState<FileList | null>(null)
+  const[allFiles, setAllFiles] = useState<FileList | null |[] >(null)
+  const [error,setError]= useState(false)
+  const data: any[]=[]  
 
-  
   const changeFileInput=(event:React.ChangeEvent<HTMLInputElement>)=>{
     let files = event.target.files;
     setAllFiles(files)
   }
-  
+ 
   const uploadMultipleFiles = async (e:any)=>{
     e.preventDefault()
-    console.log(allFiles)
     let form= new FormData(e.currentTarget)
     let mainFiles= allFiles!
-    form.append('name', 'sdjksdjks')
-    console.log(20, mainFiles)
     for(let i=0; i<mainFiles.length!; i++){
-      console.log(22,mainFiles[i])
       form.append('files', mainFiles[i])
-      console.log(24, form)
     }
-    
-    const poster= await axios.post('http://localhost:3000/addFiles', form, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    })
-    console.log(poster.data)
+
+    for(let i=0; i<mainFiles.length;i++){
+      if(mainFiles[i].type !="application/pdf"){
+       
+         data.push(mainFiles[i])
+      }
+    }
+    console.log(data)
+
+    if(data.length<=0){
+      setError((prev)=>prev=false)
+      const poster= await axios.post('http://localhost:3000/addFiles', form, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+     console.log(poster.data())
+    }
+    else{
+      setError((prev)=>prev=true)
+      console.log('error found')
+    }
   }
+
+
 
   return (
 
@@ -40,7 +53,8 @@ function App() {
           {" "}
           Using the <i>axiois</i> to upload multiple files on NodeJS server.{" "}
         </h2>
-        <input type="file" multiple onChange={(event)=> changeFileInput(event)} />
+        <input type="file" multiple accept='.pdf' onChange={(event)=> changeFileInput(event)} onClick={()=>setError(false)}/>
+        {error ==true && <p>Error found. please insert a pdf file</p>}
         <button>
           Send Multiple Files to the Server
         </button>
